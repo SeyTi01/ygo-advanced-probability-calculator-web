df95096dd7d64066bd7d780015ef3a598cdd2bd7
SeyTi01 <jannis.ridder@gmail.com>
2025-08-02 20:08:03 +0200

    feat: add FileService implementation for async file operations

---
+++ YGOProbabilityCalculatorBlazor/Services/Shared/FileService.cs
@@ -0,0 +1,11 @@
+using YGOProbabilityCalculatorBlazor.Services.Interface;
+
+namespace YGOProbabilityCalculatorBlazor.Services.Shared;
+
+public class FileService : IFileService {
+    public Task WriteAllTextAsync(string path, string content) =>
+        File.WriteAllTextAsync(path, content);
+
+    public Task<string> ReadAllTextAsync(string path) =>
+        File.ReadAllTextAsync(path);
+}
