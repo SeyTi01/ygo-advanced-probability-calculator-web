3ae895d52c6285a6afcf1f0d50445f867e286216
SeyTi01 <jannis.ridder@gmail.com>
2025-08-03 00:25:26 +0200

    fix: card names resetting when editing

--- YGOProbabilityCalculatorBlazor/Models/Card.cs
+++ YGOProbabilityCalculatorBlazor/Models/Card.cs
@@ -4,4 +4,8 @@ public class Card(IEnumerable<CategoryBase> categories, int copies = 1, string? 
     public int Copies { get; } = copies;
     public List<CategoryBase> Categories { get; } = categories.ToList();
     public string? Name { get; } = name;
+
+    public Card WithName(string? name) => new(Categories, Copies, name);
+    public Card WithCopies(int copies) => new(Categories, copies, Name);
+    public Card WithCategories(IEnumerable<CategoryBase> categories) => new(categories, Copies, Name);
 }
