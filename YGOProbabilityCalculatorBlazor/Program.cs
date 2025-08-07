939e563467280a8d16cc88faa212cea7ffec703a
SeyTi01 <jannis.ridder@gmail.com>
2025-08-07 20:34:32 +0200

    refactor: encapsulate `Program.cs` logic in `Main` method within a static class

--- YGOProbabilityCalculatorBlazor/Program.cs
+++ YGOProbabilityCalculatorBlazor/Program.cs
@@ -1,24 +1,29 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using YGOProbabilityCalculatorBlazor;
 using YGOProbabilityCalculatorBlazor.Services.DeckImport;
 using YGOProbabilityCalculatorBlazor.Services.Interface;
 using YGOProbabilityCalculatorBlazor.Services.ProbabilityCalculator;
 using YGOProbabilityCalculatorBlazor.Services.Session;
 using YGOProbabilityCalculatorBlazor.Services.Shared;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace YGOProbabilityCalculatorBlazor;
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IDeckImportService, DeckImportService>();
-builder.Services.AddScoped<ISessionService, SessionService>();
-builder.Services.AddScoped<ICardInfoService, CardInfoService>();
-builder.Services.AddScoped<IFileService, FileService>();
-builder.Services.AddScoped<ISerializer, JsonSerializer>();
-builder.Services.AddScoped<IPendingSessionService, PendingSessionService>();
-builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
-builder.Services.AddScoped<IProbabilityCalculatorService, ProbabilityCalculatorService>();
+public static class Program {
+    public static async Task Main(string[] args) {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-await builder.Build().RunAsync();
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped<IDeckImportService, DeckImportService>();
+        builder.Services.AddScoped<ISessionService, SessionService>();
+        builder.Services.AddScoped<ICardInfoService, CardInfoService>();
+        builder.Services.AddScoped<IFileService, FileService>();
+        builder.Services.AddScoped<ISerializer, JsonSerializer>();
+        builder.Services.AddScoped<IPendingSessionService, PendingSessionService>();
+        builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
+        builder.Services.AddScoped<IProbabilityCalculatorService, ProbabilityCalculatorService>();
+
+        await builder.Build().RunAsync();
+    }
+}
