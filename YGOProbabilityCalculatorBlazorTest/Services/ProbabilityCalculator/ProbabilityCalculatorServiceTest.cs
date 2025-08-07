942d9ca2ba51933ca70cca11de69524a97b735d9
SeyTi01 <jannis.ridder@gmail.com>
2025-08-07 05:25:35 +0200

    refactor: rename `ProbabilityCalculator` to `ProbabilityCalculatorService` to avoid namespace conflicts

--- YGOProbabilityCalculatorBlazorTest/Services/ProbabilityCalculator/ProbabilityCalculatorServiceTest.cs
+++ YGOProbabilityCalculatorBlazorTest/Services/ProbabilityCalculator/ProbabilityCalculatorServiceTest.cs
@@ -1,10 +1,10 @@
 using YGOProbabilityCalculatorBlazor.Models;
-using CalcServices = YGOProbabilityCalculatorBlazor.Services.ProbabilityCalculator.ProbabilityCalculator;
+using YGOProbabilityCalculatorBlazor.Services.ProbabilityCalculator;
 
 namespace YGOProbabilityCalculatorBlazorTest.Services.ProbabilityCalculator;
 
 [TestFixture]
-public class ProbabilityCalculatorTest {
+public class ProbabilityCalculatorServiceTest {
     private const double Tolerance = 1e-12;
 
     [Test]
@@ -17,7 +17,7 @@ public class ProbabilityCalculatorTest {
 
         var categories = new[] { new Category("A", 0, 0) };
 
-        var probability = CalcServices.CalculateProbabilityForCategories(deck, categories, 2);
+        var probability = ProbabilityCalculatorService.CalculateProbabilityForCategories(deck, categories, 2);
 
         Assert.That(probability, Is.EqualTo(1.0 / 6.0).Within(Tolerance));
     }
@@ -47,7 +47,7 @@ public class ProbabilityCalculatorTest {
             new Category("C", 0, handSize)
         };
 
-        var probability = CalcServices.CalculateProbabilityForCategories(deck, categories, handSize);
+        var probability = ProbabilityCalculatorService.CalculateProbabilityForCategories(deck, categories, handSize);
         Assert.That(probability, Is.EqualTo(1.0).Within(Tolerance));
     }
 
@@ -75,8 +75,8 @@ public class ProbabilityCalculatorTest {
 
         var categories = new[] { new Category("A", 1, 1) };
 
-        var singleProb = CalcServices.CalculateProbabilityForCategories(deck, categories, handSize);
-        var comboProb = CalcServices.CalculateProbabilityForCombos(deck, [comboA, comboAb], handSize);
+        var singleProb = ProbabilityCalculatorService.CalculateProbabilityForCategories(deck, categories, handSize);
+        var comboProb = ProbabilityCalculatorService.CalculateProbabilityForCombos(deck, [comboA, comboAb], handSize);
 
         Assert.That(comboProb, Is.EqualTo(singleProb).Within(Tolerance));
     }
@@ -100,7 +100,7 @@ public class ProbabilityCalculatorTest {
             new ComboCategory(categoryC, 1, 1)
         ]);
 
-        var probability = CalcServices.CalculateProbabilityForCombos(cards, [combo1, combo2], 2);
+        var probability = ProbabilityCalculatorService.CalculateProbabilityForCombos(cards, [combo1, combo2], 2);
         Assert.That(probability, Is.EqualTo(0.5 + 1.0 / 6.0).Within(Tolerance));
     }
 
@@ -120,7 +120,7 @@ public class ProbabilityCalculatorTest {
             new Category("extender", 1, 1)
         };
 
-        var probability = CalcServices.CalculateProbabilityForCategories(deck, requirements, 2);
+        var probability = ProbabilityCalculatorService.CalculateProbabilityForCategories(deck, requirements, 2);
         Assert.That(probability, Is.EqualTo(5.0 / 6.0).Within(Tolerance));
     }
 
@@ -137,7 +137,7 @@ public class ProbabilityCalculatorTest {
 
         var requirements = new[] { new Category("starter", 2, handSize) };
 
-        var probability = CalcServices.CalculateProbabilityForCategories(deck, requirements, handSize);
+        var probability = ProbabilityCalculatorService.CalculateProbabilityForCategories(deck, requirements, handSize);
         Assert.That(probability, Is.EqualTo(7.0 / 10.0).Within(Tolerance));
     }
 
@@ -163,7 +163,7 @@ public class ProbabilityCalculatorTest {
             new Category("combo", 1, handSize)
         };
 
-        var probability = CalcServices.CalculateProbabilityForCategories(deck, categories, handSize);
+        var probability = ProbabilityCalculatorService.CalculateProbabilityForCategories(deck, categories, handSize);
         Assert.That(probability, Is.EqualTo(7.0 / 10.0).Within(Tolerance));
     }
 }
